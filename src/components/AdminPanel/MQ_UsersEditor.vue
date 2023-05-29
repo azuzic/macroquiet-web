b014fd4e9d4e6fa0b58629be0f967535bf4a82a4
Alesandro Zuzic <azuzic@student.unipu.hr>
2023-05-29 23:23:48 +0200

    Fixed admin users

--- src/components/AdminPanel/MQ_UsersEditor.vue
+++ src/components/AdminPanel/MQ_UsersEditor.vue
@@ -9,21 +9,14 @@
                 <div v-for="(user, i) in allUsers" class="flex flex-col text-MQ_light justify-center items-start">
                     <div class="flex text-MQ_light justify-start items-center">
 
-                        <img class="h-7 aspect-square rounded-full mr-1" :src="user.register_method == 'MacroQuiet' ? macroQuietIcon : googleIcon">
+                        <img class="h-6 md:h-7 aspect-square rounded-full mr-1" :src="user.register_method == 'MacroQuiet' ? macroQuietIcon : googleIcon">
 
-                        <div class="text-MQ_light font-bold text-2xl w-12 text-right mr-4">{{ i + 1 + "." }}</div>
-                        <img class="h-7 aspect-square rounded-full mr-6" :src="user.profile.image.avatar != '' ? user.profile.image.avatar : defaultUserIcon" alt=""> 
+                        <div class="text-MQ_light font-bold text-lg md:text-2xl w-8 md:w-12 text-right mr-2 md:mr-4">{{ i + 1 + "." }}</div>
+                        <img class="h-6 md:h-7 aspect-square rounded-full mr-1 md:mr-6" :src="user.profile.image.avatar != '' ? user.profile.image.avatar : defaultUserIcon" alt=""> 
  
-                        <RouterLink :to="'/user/'+ user.username">
-                            <div class="text-MQ_red text-xl font-bold hover:underline cursor-pointer w-48 text-left"> {{ user.username }} </div> 
+                        <RouterLink :to="'/users/'+ user.username">
+                            <div class="text-MQ_red text-sm md:text-xl font-bold hover:underline cursor-pointer w-48 text-left"> {{ user.username }} </div> 
                         </RouterLink>
-
-                        <div v-for="(game, i) in user.profile.games" class="flex text-lg mr-2"> 
-                            <div class="mr-1 font-bold"> {{ i+1+"." }}</div>
-                            <RouterLink :to="'/games/' + game.name">
-                                <div class="text-MQ_red hover:underline cursor-pointer"> {{ game.name }} </div>
-                            </RouterLink>
-                        </div>
                         
                     </div>
                     <hr class="border-t-[1px] border-MQ_light border-opacity-25 relative z-10 w-full mt-1"/>
@@ -39,12 +32,17 @@ import defaultUserIcon from "@/assets/portraits/default_user_icon.png";
 import macroQuietIcon from "@/assets/Logos/macroquiet_logo_icon.png";
 import googleIcon from "@/assets/icons/logIn/google.svg";
 import { Admin } from "@/services";
+import { useGlobalStore } from '@/stores/globalStore';
+import { useCarouselStore } from '@/stores/carouselStore';
 
 export default {
     name: "MQ_UsersEditor",
     components: { MQ_h2_small },
     data() { return { allUsers: "", } },
-    setup() { return { defaultUserIcon, macroQuietIcon, googleIcon } },
+    setup() { 
+        const globalStore = useGlobalStore();
+        const carouselStore = useCarouselStore();
+        return { defaultUserIcon, macroQuietIcon, googleIcon, globalStore, carouselStore } },
     async mounted() {
         this.allUsers = await Admin.getData("users?=");
         this.allUsers = this.allUsers.data;
